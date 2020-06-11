7f92538b5e73b6caeb50a09ca7fe1b3c6f3fb9b1
Crescenzo De Martino <cresckamina@gmail.com>
2020-06-11 16:41:10 +0200

    Migliorata sezione lingue e aggiunta funzione scarica app

--- js/script.js
+++ js/script.js
@@ -1,25 +1,42 @@
-// Aggiungo funzionalità al passaggio del mouse sulla sezione
-$('.with-dropdown').mouseenter(
+$(document).ready(
   function () {
-    $(this).children('.dropdown').removeClass('hidden')
-  }
-)
-// Aggiungo funzionalità all'allontanamento del mouse dalla sezione
-$('.with-dropdown').mouseleave(
-  function () {
-    $(this).children('.dropdown').addClass('hidden')
-  }
-)
-// Aggiungo funzionalità al click del mouse sulla sezione
-$('.with-dropdown').click(
-  function () {
-    $(this).children('.dropdown').toggleClass('hidden')
-  }
-)
-// Aggiungo funzionalità al click del mouse sulla lingua
-$('.lang').click(
-  function () {
-    $('.lang i').toggleClass('fa-angle-up')
-    $('.lang i').toggleClass('fa-angle-down')
+    // Aggiungo funzionalità al passaggio del mouse sulla sezione
+    $('.with-dropdown').mouseenter(
+      function () {
+        $(this).children('.dropdown').removeClass('hidden')
+      }
+    )
+    // Aggiungo funzionalità all'allontanamento del mouse dalla sezione
+    $('.with-dropdown').mouseleave(
+      function () {
+        $(this).children('.dropdown').addClass('hidden')
+      }
+    )
+    // Aggiungo funzionalità al click del mouse sulla sezione
+    $('.with-dropdown').click(
+      function () {
+        $(this).children('.dropdown').toggleClass('hidden')
+      }
+    )
+    // Aggiungo funzionalità al click del mouse sulla lingua
+    $('.with-dropdown-lang').click(
+      function () {
+        $('.with-dropdown-lang i').toggleClass('fa-angle-up')
+        $('.with-dropdown-lang i').toggleClass('fa-angle-down')
+        $(this).children('.dropdown').toggleClass('hidden')
+      }
+    )
+    // Aggiungo funzionalità al click del tasto scarica app
+    $('.scarica').click(
+      function () {
+        $('.downloadPage').removeClass('hidden')
+      }
+    )
+    // Aggiungo funzionalità al click del tasto chiudi pagina scarica app
+    $('.downloadPage > i').click(
+      function () {
+        $('.downloadPage').addClass('hidden')
+      }
+    )
   }
 )
